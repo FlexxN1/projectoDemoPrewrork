ac1074816fb537c83a46efc09f9811e6f9f11b1d
flexx <flexx@Flexx.localdomain>
2021-04-28 23:11:11 -0500

    agrege nuevos index :v

---
+++ index.js
@@ -0,0 +1,12 @@
+const name = ["ricardo", "paola", "prechet"];
+const apellido = ["sam", "andrade", "perst"];
+
+function saludarPerosnas(name, apellido){
+    this.name = name;
+    this.apellido = apellido;
+};
+
+for(const i = 0; i < name.lenth, i < apellido.length; i++){
+    const nuevoAprendiz = new saludarPersonas(name[i], apellido[i]);
+    console.log(nuevoAprendiz);
+};
